e9727431e0058efce66ef7408b31116223abf845
Arthur Koziel <arthur@arthurkoziel.com>
2009-02-02 12:18:43 +0100

    cleaned up setup.py, renamed package do "disqus", readme updates

--- README.markdown
+++ README.markdown
@@ -1,45 +1,56 @@
-django-disqus
-=============
+# django-disqus
 
 **WORK IN PROGRESS!**
 
-Features
---------
+## Features
 
 * Export comments from django.contrib.comments to DISQUS.
 * Dump data from DISQUS in local JSON file.
+* Templatetags
 
-Requirements
-------------
+## Requirements
 
- * Django 1.0.2
+ * Django 1.0
 
-Installation
-------------
+## Installation
 
-1. Add 'django\_disqus' to your INSTALLED\_APPS.
+1. Add 'disqus' to your INSTALLED\_APPS.
 2. Add "DISQUS\_API\_KEY" and "DISQUS\_WEBSITE\_SHORTNAME" to your settings file. You can [http://disqus.com/api/get_my_key/][get your API key here].
 
-Usage
------
+## Usage
+
+### Exporting comments
 
 To export comments to disqus:
 
     python manage.py disqus-export
 
+### Dumping data
+
 To dump the data from disqus:
     
     python manage.py disqus-dumpdata
-    
+
 You can pass the --indent option to specify the indentation of the output:
     
     python manage.py disqus-dumpdata --indent=4
+
+### Templatetags
+
+The following templatetags are available:
     
-Troubleshooting
----------------
+- **disqus\_dev**: Returns js that sets variables to display comments on local dev server
+- **disqus\_num\_replies**: Returns js to display comment count
+- **disqus\_show\_comments**: Returns js to display comment form
+    
+## Troubleshooting
 
 Make sure that:
 
 * Each content object has a get\_absolute\_url() method.
 * Your Site has the correct domain set.
 * The content object has a __unicode__ method
+
+## TODO
+
+* `is_usable` for templatetags
--- setup.py
+++ setup.py
@@ -1,52 +1,22 @@
 #!/usr/bin/env python
 
-import os
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup, find_packages
 
-app_name = 'django_disqus'
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk(app_name):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[len(app_name)+1:] # Strip "app_name/" or "app_name\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-setup(name='django-'+app_name,
-      version='0.1',
-      description='Import and export from Disqus',
-      author='Arthur Koziel',
-      author_email='arthur@arthurkoziel.com',
-      url='http://arthurkoziel.com',
-      packages=packages,
-      package_dir={app_name: app_name},
-      package_data={app_name: data_files},
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+setup(
+    name='django-disqus',
+    version='0.1',
+    description='DISQUS commands for Django',
+    author='Arthur Koziel',
+    author_email='arthur@arthurkoziel.com',
+    url='http://github.com/arthurk/django-disqus',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Plugins',
+      'Framework :: Django',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Programming Language :: Python',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=find_packages(),
+)
