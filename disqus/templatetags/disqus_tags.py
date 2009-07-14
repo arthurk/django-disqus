97358478e1136f2b37e3813e836ac2303d7cff43
Arthur Koziel <arthur@arthurkoziel.com>
2009-07-14 18:53:50 +0200

    [PATCH] Added <p> tags to make output pass XHTML validation; added optional override of title, URL, and page snippet in disqus_show_comments; added a disqus_recent_comments tag for displaying the Disqus Recent Comments widget. Thanks David Roberts.

--- disqus/templatetags/disqus_tags.py
+++ disqus/templatetags/disqus_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.defaultfilters import escapejs
 from django.conf import settings
 from django.contrib.sites.models import Site
 
@@ -28,29 +29,48 @@ def disqus_num_replies():
     //<![CDATA[
     (function() {
         var links = document.getElementsByTagName('a');
-    	var query = '?';
-    	for(var i = 0; i < links.length; i++) {
-    		if(links[i].href.indexOf('#disqus_thread') >= 0) {
-    			query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
-    		}
-    	}
-    	document.write('<script type="text/javascript" src="http://disqus.com/forums/%s/get_num_replies.js' + query + '"></' + 'script>');
+        var query = '?';
+        for(var i = 0; i < links.length; i++) {
+            if(links[i].href.indexOf('#disqus_thread') >= 0) {
+                query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+            }
+        }
+        document.write('<script type="text/javascript" src="http://disqus.com/forums/%s/get_num_replies.js' + query + '"></' + 'script>');
     })();
     //]]>
     </script>
     """ % settings.DISQUS_WEBSITE_SHORTNAME
 
-def disqus_show_comments():
+def disqus_show_comments(title=None, url=None, snippet=None):
     """
     Returns the HTML code necessary to display DISQUS comments.
     """
-    return """
+    if title or url or snippet:
+        s = '<script type="text/javascript">'
+        if title:   s += 'var disqus_title = "%s";' % escapejs(title)
+        if url:     s += 'var disqus_url = "http://%s%s";' % \
+                        (Site.objects.get_current().domain, escapejs(url))
+        if snippet: s += 'var disqus_message = "%s";' % escapejs(snippet)
+        s += '</script>'
+    else: s = ''
+    return s + """
     <div id="disqus_thread"></div>
     <script type="text/javascript" src="http://disqus.com/forums/%(shortname)s/embed.js"></script>
-    <noscript><a href="http://%(shortname)s.disqus.com/?url=ref">View the discussion thread.</a></noscript>
-    <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+    <noscript><p><a href="http://%(shortname)s.disqus.com/?url=ref">View the discussion thread.</a></p></noscript>
+    <p><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a></p>
     """ % dict(shortname=settings.DISQUS_WEBSITE_SHORTNAME)
+
+def disqus_recent_comments(num_items=3, avatar_size=32):
+    """
+    Returns the HTML/js code necessary to display the recent comments widget.
+    """
+    return """
+    <script type="text/javascript" src="http://disqus.com/forums/%(shortname)s/recent_comments_widget.js?num_items=%(num_items)d&amp;avatar_size=%(avatar_size)d"></script>
+    <noscript><p><a href="http://%(shortname)s.disqus.com/?url=ref">View the discussion thread.</a></p></noscript>
+    """ % dict(shortname=settings.DISQUS_WEBSITE_SHORTNAME,
+               num_items=num_items, avatar_size=avatar_size)
 
 register.simple_tag(disqus_dev)
 register.simple_tag(disqus_num_replies)
 register.simple_tag(disqus_show_comments)
+register.simple_tag(disqus_recent_comments)
