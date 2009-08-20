4f34feec99362e61a52745835298cbe7eb3abd49
Arthur Koziel <arthur@arthurkoziel.com>
2009-08-20 17:24:00 +0200

    remove new api for now, as it wasnt being used anyway; prep for 0.2 release

--- setup.py
+++ setup.py
@@ -4,20 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-disqus',
-    version='0.2dev',
+    version='0.2',
     description='Easily integrate DISQUS comments into your Django website',
     author='Arthur Koziel',
     author_email='arthur@arthurkoziel.com',
     url='http://github.com/arthurk/django-disqus',
     license='New BSD License',
     classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Environment :: Plugins',
       'Framework :: Django',
       'Intended Audience :: Developers',
       'License :: OSI Approved :: BSD License',
       'Programming Language :: Python',
-      'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(),
     zip_safe=False,
