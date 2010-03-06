74c64a2514aa27cd8253ade64f4eb78bcaf0f3d9
Arthur Koziel <arthur@arthurkoziel.com>
2010-03-06 13:46:41 +0100

    sphinx test

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({desctypes:{},terms:{index:0,search:0,welcom:0,modul:0,indic:0,django:0,content:0,tabl:0,document:0,page:0,disqu:0},titles:["Welcome to django-disqus&#8217;s documentation!"],modules:{},descrefs:{},filenames:["index"]})
