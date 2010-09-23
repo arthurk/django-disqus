86e10b0db4729b5102c17f29e2ef4278f6ea90f8
David Cramer <dcramer@gmail.com>
2010-09-23 15:12:56 -0700

    basic test runner

---
+++ runtests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import logging
+import sys
+from os.path import dirname, abspath
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure(
+        DATABASE_ENGINE='sqlite3',
+        INSTALLED_APPS=[
+            'django.contrib.auth',
+            'django.contrib.admin',
+            'django.contrib.sessions',
+            'django.contrib.contenttypes',
+            'disqus',
+        ],
+        ROOT_URLCONF='',
+        DEBUG=False,
+    )
+
+from django.test.simple import run_tests
+
+
+def runtests(*test_args):
+    if not test_args:
+        test_args = ['disqus']
+    parent = dirname(abspath(__file__))
+    sys.path.insert(0, parent)
+    failures = run_tests(test_args, verbosity=1, interactive=True)
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
