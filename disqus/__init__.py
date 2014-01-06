5307c349384f1ca36248428cfbf675e7680dbb13
fadur <iam@feisal.dk>
2014-01-06 03:35:54 -0800

    #26 Django 1.6 Deprecation Warning

--- disqus/__init__.py
+++ disqus/__init__.py
@@ -2,7 +2,7 @@ import urllib
 import urllib2
 
 from django.core.management.base import CommandError
-from django.utils import simplejson as json
+import json
 from django.conf import settings
 
 def call(method, data, post=False):
