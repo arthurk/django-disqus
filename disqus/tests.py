6a7bd021adca6fc8924478455c20035e08429110
Chris Wilson <chris+github@aptivate.org>
2014-01-09 12:18:49 +0000

    Replace doctests with unittest
    For now we test the same as the old doctests

--- disqus/tests.py
+++ disqus/tests.py
@@ -1,28 +1,23 @@
+import unittest
+
 from django.conf import settings
 from django.core.management.base import CommandError
+from unittest import TestCase
 
 from disqus.api import DisqusClient
 
-__test__ = {'API_TESTS': """
+class DisqusTest(TestCase):
+    def test_client_init(self):
+        """
+        First, we test if the DisqusClient class can be initialized
+        and parameters that were passed are set correctly.
+        """
 
-First, we test if the DisqusClient class can be initialized
-and parameters that were passed are set correctly.
+        c = DisqusClient(foo='bar', bar='foo')
+        self.assertEqual('bar', c.foo)
+        self.assertEqual('foo', c.bar)
+        with self.assertRaises(AttributeError):
+            c.baz
 
->>> c = DisqusClient(foo='bar', bar='foo')
->>> c.foo
-'bar'
->>> c.bar
-'foo'
->>> c.baz
-Traceback (most recent call last):
-    ...
-AttributeError
-
-
-When a DISQUS API method is called, the call method should be used.
-
->>> c.get_forum_list
-<function call_method at ...>
-""",
-}
-
+if __name__ == '__main__':
+    unittest.main()
