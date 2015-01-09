309351becb7cea090b26cf3d9ecf989be69b987a
Alkalit <lyoffa412@gmail.com>
2015-01-09 13:07:11 +0500

    Small fix in test.py

--- disqus/tests.py
+++ disqus/tests.py
@@ -14,12 +14,14 @@ from disqus.templatetags.disqus_tags import disqus_dev
 
 
 class FakeRequest(object):
+
     def __init__(self, path):
         self.path = path
 
 
 # mock Site
 class FakeSiteManager(object):
+
     def __init__(self, domain, name):
         self.site = Site(domain=domain, name=name)
 
@@ -42,20 +44,23 @@ class DisqusTest(TestCase):
         """
 
         c = DisqusClient(foo='bar', bar='foo')
+
         self.assertEqual('bar', c.foo)
         self.assertEqual('foo', c.bar)
         with self.assertRaises(AttributeError):
             c.baz
 
+    @override_settings(DEBUG=True)
     def test_disqus_dev_sets_full_url(self):
         test_domain = 'example.org'
         url_path = '/path/to/page'
         full_url = '//%s%s' % (test_domain, url_path)
         context = {'request': FakeRequest(path=url_path)}
+
         # mock out Site manager
         Site.objects = FakeSiteManager(test_domain, 'test')
-        with override_settings(DEBUG=True):
-            generated_html = disqus_dev(context)
+        generated_html = disqus_dev(context)
+
         self.assertIn(full_url, generated_html)
 
 if __name__ == '__main__':
